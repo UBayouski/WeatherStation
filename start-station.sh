7d2c4d41c816e75c295f6cec4450e6a3f1fa33a4
UBayouski <uladzislaubayouski@gmail.com>
2017-04-20 17:48:32 -0700

    Update start-station.sh

--- start-station.sh
+++ start-station.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Raspberry Pi + Raspbian Weather Station
+# By Uladzislau Bayouski
+# https://www.linkedin.com/in/uladzislau-bayouski-a7474111b/
+
 # How To Autostart Apps In Rasbian LXDE Desktop
 # http://www.raspberrypi-spy.co.uk/2014/05/how-to-autostart-apps-in-rasbian-lxde-desktop/
 
