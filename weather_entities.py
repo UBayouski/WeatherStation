fa5b7e07917d9e5afaa063302b5a8a188509cb98
UBayouski <uladzislaubayouski@gmail.com>
2017-04-20 17:47:18 -0700

    Update weather_entities.py

--- weather_entities.py
+++ weather_entities.py
@@ -1,20 +1,41 @@
+#!/usr/bin/python
+'''*****************************************************************************************************************
+    Raspberry Pi + Raspbian Weather Station
+    By Uladzislau Bayouski
+    https://www.linkedin.com/in/uladzislau-bayouski-a7474111b/
+    
+    Weather entities (temperature, humidity, pressure) package.
+********************************************************************************************************************'''
+
 from abc import ABCMeta, abstractmethod, abstractproperty
 
 from config import Config
 from visual_styles import ArrowStyle, NumericStyle, SquareStyle, VisualStyle
 
 class CarouselContainer(object):
+    """
+    Base class for carousel like classes.
+    
+    These classes contain list of items, and iterate through them functionality.
+    """
     __metaclass__ = ABCMeta
 
     def __init__(self):
+        # Index of current carousel item
         self.current_index = 0
 
     @abstractproperty
     def carousel_items(self):
+        """Carousel items to iterate through."""
         pass
    
     @property
     def next_item(self):
+        """
+        Gets next item, and sets its index as current_index.
+        
+        If item is the last one returns the first item.
+        """
         if self.current_index < len(self.carousel_items) - 1:
             self.current_index += 1
         else:
@@ -24,6 +45,11 @@ class CarouselContainer(object):
 
     @property
     def previous_item(self):
+        """
+        Gets previous item, and sets its index as current_index.
+        
+        If item is the first one returns the last item.
+        """
         if self.current_index > 0:
             self.current_index -= 1
         else:
@@ -33,17 +59,21 @@ class CarouselContainer(object):
 
     @property
     def current_item(self):
+        """Returns current item."""
         return self.carousel_items[self.current_index]
 
 class WeatherEntityType(object):
+    """Enum object for weather enitites types."""
     HUMIDITY, PRESSURE, TEMPERATURE = range(3)
 
 class WeatherEntity(CarouselContainer):
+    """Base class for weather entities implementations."""
     __metaclass__ = ABCMeta
 
     def __init__(self):
         super(WeatherEntity, self).__init__()
 
+        # Default set of visual styles for weather entity instance
         self._visual_styles = (
             NumericStyle(self.positive_color, self.negative_color),
             ArrowStyle(self.positive_color, self.negative_color),
@@ -52,18 +82,22 @@ class WeatherEntity(CarouselContainer):
 
     @abstractproperty
     def entity_messsage(self):
+        """Message to show when switching to it."""
         pass
 
     @abstractproperty
     def positive_color(self):
+        """Color to use for positive values."""
         pass
 
     @abstractproperty
     def negative_color(self):
+        """Color to use for negative values."""
         pass
 
     @abstractproperty
     def entity_type(self):
+        """Returns WeatherEntityType value."""
         pass
 
     @property
@@ -72,12 +106,15 @@ class WeatherEntity(CarouselContainer):
 
     @property
     def current_style(self):
+        """Current applied visual style."""
         return self.current_item
 
     def show_pixels(self, value):
+        """Shows pixel for current entity and applied visual style."""
         return self.current_item.apply_style(value)
 
 class HumidityEntity(WeatherEntity):
+    """Humidity enity implementation."""
 
     def __init__(self):
         super(HumidityEntity, self).__init__()
@@ -99,12 +136,15 @@ class HumidityEntity(WeatherEntity):
         return WeatherEntityType.HUMIDITY
 
     def show_pixels(self, value):
+        # For square visual style we divide by 100 and multiply by 64 (8x8 screen resolution) 
+        # because humidity value is in percent
         if self.current_style is SquareStyle:
             value = 64 * value / 100
 
         return super(HumidityEntity, self).show_pixels(value)
 
 class PressureEntity(WeatherEntity):
+    """Pressure enity implementation."""
 
     def __init__(self):
         super(PressureEntity, self).__init__()
@@ -125,7 +165,8 @@ class PressureEntity(WeatherEntity):
     def entity_type(self):
         return WeatherEntityType.PRESSURE
 
-class TemperatureEntity(WeatherEntity):
+class TemperatureEntity(WeatherEntity):    
+    """Temperature enity implementation."""
 
     def __init__(self):
         super(TemperatureEntity, self).__init__()
